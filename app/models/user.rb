b42250885a1f93988a2053c12bca619f517f1e62
EC2 Default User <ec2-user@ip-172-31-42-252.ap-northeast-1.compute.internal>
2019-11-01 23:23:29 +0000

    rails04_02

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,13 @@
 class User < ApplicationRecord
-  validates :name, presence: true
-  validates :email, presence: true
+  validates :name, presence: true, length: { maximum: 15 }
+  
+  EMAIL_REGEX = /[a-zA-Z0-9._+]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*/
+  validates :email, presence: true, format: { with: EMAIL_REGEX }
+  
+  validates :password, length: { in: 8..32 }
+  
+  PW_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{8,32}\z/i
+  validates :password, presence: true, format: { with: PW_REGEX }
   
   has_secure_password
 end
